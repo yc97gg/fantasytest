394e6a3d346a2980e5c7b2eecedcab8b4159a6bd
yc97gg <yc97gg@gmail.com>
2025-04-29 16:00:24 +0800

    unity andconfig
    unity和config

---
+++ Client/Assets/scripts/Entry.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Fantasy;
+using UnityEngine;
+using Fantasy.Async;
+
+public class Entry : MonoBehaviour
+{
+    Scene scene;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //初始化框架
+        Fantasy.Platform.Unity.Entry.Initialize(GetType().Assembly);
+        StartAsync().Coroutine();
+    }
+
+    private async FTask StartAsync()
+    {
+        //创建一个游戏scene
+        scene = await Fantasy.Platform.Unity.Entry.CreateScene();
+
+       
+    }
+}
