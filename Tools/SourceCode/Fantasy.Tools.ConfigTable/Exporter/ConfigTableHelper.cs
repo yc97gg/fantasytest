dc1ca265e22ce1221f95610ca613308f3157c1f6
yc97gg <yc97gg@gmail.com>
2025-04-29 14:53:29 +0800

    tools
    fantasytools

---
+++ Tools/SourceCode/Fantasy.Tools.ConfigTable/Exporter/ConfigTableHelper.cs
@@ -0,0 +1,13 @@
+using Fantasy.Serialize;
+
+#pragma warning disable CS8603 // Possible null reference return.
+
+namespace Fantasy;
+
+public static class ConfigTableHelper
+{
+    public static T Load<T>() where T : ASerialize
+    {
+        return default;
+    }
+}
